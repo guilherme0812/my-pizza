9dda0e0b34759e93b36d993003fbd94130c0a03a
guilherme eduardo <guilherme09112001@gmail.com>
2022-06-12 21:38:53 -0300

    stilizando componente de card de message

--- src/screens/Home/components/NoticeCard/index.jsx
+++ src/screens/Home/components/NoticeCard/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native'
-import styles from './styles';
+import { View, Image } from 'react-native'
+import {Container, CardMessageConatiner, TextCard} from './styles';
 import { useHome } from '../../context/Home';
 
 const NoticeCard = () => {
   const { cardMessage } = useHome()
   return (
-    <View style={styles.noticeCardContainer}>
+    <Container>
       <View>
       <Image
         source={require('../../../../assets/images/moto.png')}
       />
       </View>
-      <View style={[styles.cardMessageContainer, styles.shadow]}>
-        <Text style={[styles.bold, styles.text2Xl, styles.cardMessage]}> {cardMessage} </Text>
-      </View>
-    </View>
+      <CardMessageConatiner>
+        <TextCard> {cardMessage} </TextCard>
+      </CardMessageConatiner>
+    </Container>
   )
 }
 export default NoticeCard
--- src/screens/Home/components/NoticeCard/styles.jsx
+++ src/screens/Home/components/NoticeCard/styles.jsx
@@ -1,33 +1,23 @@
-import { StyleSheet } from "react-native";
-import { global, colors } from "../../../../assets/css"
+import styled from 'styled-components/native'
+import { Text } from "../../../../components";
 
-const styles = StyleSheet.create({
-  ...global,
-  noticeCardContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 20,
-    backgroundColor: '#ffddce',
-    borderRadius: 10,
-    marginTop: 40,
-    marginBottom: 40,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-  },
-  cardMessageContainer: {
-    width: '60%'
-  },
-  cardMessage: {
-    color: colors.orange1,
-    textAlign: 'left'
-  },
-  shadow: {
-    shadowOffset: { width: 10, height: 10, },
-    shadowColor: '#000000',
-    shadowOpacity: 1.0,
-  }
+export const Container = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: ${({theme}) => theme.padding}px;
+  background-color:#ffddce;
+  border-radius: 10px;
+  margin-left: ${({theme}) => theme.padding}px;
+  margin-right: ${({theme}) => theme.padding}px;
+`
 
-})
-export default styles
+export const CardMessageConatiner = styled.View`
+  width: 60%;
+`
+export const TextCard = styled(Text).attrs(props =>({
+  size: "6",
+  bold: true,
+}))`
+  color: ${({theme}) => theme.primary};
+  text-align: left;
+`
